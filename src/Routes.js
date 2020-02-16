6c2ebbdffa7226c01b6f49a7d9fb7b4f05d20d26
yeonghyeon <elitebrain@naver.com>
2020-02-16 22:27:32 +0900

    initial

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StaticRouter as Router, Route, Switch } from "react-router-dom";
 
-import GlobalStyles from "styles/global";
+import GlobalStyles from "./styles/global";
 
 const Routes = () => {
   return (
