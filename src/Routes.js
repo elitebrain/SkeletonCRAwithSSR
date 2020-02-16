d9c1191e37350558f09c1a800552d0b0a34298cf
yeonghyeon <elitebrain@naver.com>
2020-02-16 22:10:51 +0900

    init ssr by CRA

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { StaticRouter as Router, Route, Switch } from "react-router-dom";
 
-import GlobalStyles from "styles/global";
+import GlobalStyles from "./styles/global";
 
 const Routes = () => {
   return (
